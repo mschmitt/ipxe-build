048121581c7679e0e4a54f76d2e21423df13e2f3
Martin Schmitt <mas@scsy.de>
2020-02-29 11:45:00 +0100

    copypasta fix

--- qemu.sh
+++ qemu.sh
@@ -2,4 +2,4 @@
 builddir="$(dirname "$(readlink -f "$0")")"
 qemu-system-x86_64 -curses -m 2G -enable-kvm \
 	-smbios type=1,manufacturer=ipxe-build,serial=d155bd71 \
-	-cdrom file="${builddir}/images/ipxe-cdrom.iso"
+	-cdrom "${builddir}/images/ipxe-cdrom.iso"
